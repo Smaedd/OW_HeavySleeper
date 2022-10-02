aa21d61bc8dc5fac8fd32825c0c7433a321893f8
Smae De <mitrblue@gmail.com>
2022-10-03 01:10:04 +1100

    feat: Initial commit

---
+++ HeavySleeper/HeavySleeper.cs
@@ -0,0 +1,51 @@
+﻿using OWML.Common;
+using OWML.ModHelper;
+using UnityEngine;
+using HarmonyLib;
+
+namespace HeavySleeper
+{
+    public class HeavySleeper : ModBehaviour
+    {
+        private float _minLoopDuration;
+        private float _maxLoopDuration;
+        public bool _lightSleeperMode;
+
+        public const float DEFAULT_LOOP_DURATION = 1320f;
+
+        public static HeavySleeper Instance;
+
+        private void Awake()
+        {
+            Instance = this;
+        }
+
+        public override void Configure(IModConfig config)
+        {
+            _minLoopDuration = config.GetSettingsValue<float>("Minimum Time Left (seconds)");
+            _maxLoopDuration = config.GetSettingsValue<float>("Maximum Time Left (seconds)");
+
+            _lightSleeperMode = config.GetSettingsValue<bool>("Light Sleeper Mode (Wake up at 0 seconds)");
+        }
+        private void Start()
+        {
+            Harmony.CreateAndPatchAll(System.Reflection.Assembly.GetExecutingAssembly());
+
+            ModHelper.Events.Player.OnPlayerAwake += (playerBody) =>
+            {
+                float timeLeft = Random.Range(_minLoopDuration, _maxLoopDuration);
+
+                if (_lightSleeperMode)
+                {
+                    // We are waking up at second 0, so we set duration rather than offset
+                    // (Offset becomes 0 in the SetSecondsRemaining call.)
+                    TimeLoop._loopDuration = timeLeft;
+                }
+
+                TimeLoop.SetSecondsRemaining(timeLeft);
+
+                ModHelper.Console.WriteLine($"Set time left to: {timeLeft}", MessageType.Debug);
+            };
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+# HeavySleeper
+Randomises when you wake up, setting loop time left to a value in a range defined in the mod settings.
+
+Also adds a second mode, 'Light Sleeper', in which the player will always wake up at the start of the loop, but the loop will have a randomised duration, giving a subtly altered experience from the default mode.
